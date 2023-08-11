184ba11be290875daf52d8fcf24d9bee5e3bfce9
Will Roden <will@roden.cc>
2023-08-11 12:44:39 -0500

    fix rc in go.mod

--- src/run_test_long.sh
+++ src/run_test_long.sh
@@ -13,8 +13,6 @@ setUp() {
 STABLE_VERSIONS_URL="https://raw.githubusercontent.com/WillAbides/goreleases/077db58ac86a8a2fb63c90817090e132eded0f3d/versions.txt"
 
 do_test_run() {
-  CONSTRAINT="$1"
-  WANT_VERSION="$2"
   tmp_dir="$SHUNIT_TMPDIR"/test_run
   rm -rf -- "$tmp_dir"
   mkdir -p -- "$tmp_dir"
@@ -23,11 +21,11 @@ do_test_run() {
   export RUNNER_WORKSPACE="$tmp_dir/runner_workspace"
   export GITHUB_OUTPUT="$tmp_dir/github_output"
   export GITHUB_ACTION_PATH="$PWD"
-  export VERSIONS_URL="${VERSIONS_URL:-$STABLE_VERSIONS_URL}"
+  export VERSIONS_URL="$STABLE_VERSIONS_URL"
   export SKIP_MATCHER=1
   export IGNORE_LOCAL_GO=1
   export GOROOT=""
-  GO_VERSION="$CONSTRAINT" ./src/run
+  ./src/run
   WANT_GOROOT="$RUNNER_WORKSPACE/setup-go-faster/go/$WANT_VERSION/x64"
   if [ "$(goos)" = "windows" ]; then
     # Windows is trickier because of how it translates paths.
@@ -38,15 +36,39 @@ do_test_run() {
 }
 
 test_run_1_15_x() {
-  do_test_run 1.15.x 1.15.15
+  GO_VERSION="1.15.x" \
+    WANT_VERSION="1.15.15" \
+    do_test_run
 }
 
 test_run_star() {
-  do_test_run '*' 1.20.7
+  GO_VERSION="*" \
+    WANT_VERSION="1.20.7" \
+    do_test_run
 }
 
 test_run_1_16rc1() {
-  do_test_run 1.16rc1 1.16rc1
+  GO_VERSION="1.16rc1" \
+    WANT_VERSION="1.16rc1" \
+    do_test_run
+}
+
+test_run_1_21rc4() {
+  GO_VERSION="1.21rc4" \
+    WANT_VERSION="1.21rc4" \
+    do_test_run
+}
+
+test_go_mod() {
+  GO_VERSION_FILE="$SHUNIT_TMPDIR"/test_go_mod/go.mod
+  mkdir -p -- "$(dirname -- "$GO_VERSION_FILE")"
+  echo "
+module foo
+go 1.20.7 // the go version
+" > "$GO_VERSION_FILE"
+  GO_VERSION_FILE="$GO_VERSION_FILE" \
+    WANT_VERSION="1.20.7" \
+    do_test_run
 }
 
 . ./external/shunit2
